87a1983db09fef96c96bbef9dbe6404a3da6d15e
Ozair-Hassan <ozair.hassan123@gmail.com>
2023-02-16 20:31:53 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from './components/ui/Header'
 import BackgroundImage from './components/ui/BackgroundImage'
@@ -6,34 +6,71 @@ import CharacterGrid from './components/characters/CharacterGrid'
 import Search from './components/ui/Search'
 import './App.css'
 
-
 const App = () => {
   const [items, setItems] = useState([])
+  const [filteredResults, setFilteredResults] = useState([])
   const [isLoading, setIsLoading] = useState([true])
   const [query, setQuery] = useState('')
+  const [searchText, setSearchText] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://thronesapi.com/api/v2/Characters/${query}`)
+      // Returns an array when no query param but returns an object when query param present
+      // Also this should be in a try-catch block because it will throw an error if the id (query param) does not exist in their database. Discuss this with me in person.
 
-      console.log(query);
-      console.log(result.data)
+      // const result = axios(`https://thronesapi.com/api/v2/Characters/${query}`)
+
+      let result
+      try {
+        result = await axios(`https://thronesapi.com/api/v2/Characters/`)
+      } catch (error) {
+        // preferably create a toast which says invalid search
+        // result = await axios(`https://thronesapi.com/api/v2/Characters`)
+        console.log(error)
+      }
+
+      // commented out these to prevent the console from being flooded and becoming harder to debug
+      // console.log(query)
+      // console.log(result.data)
+
       setItems(result.data)
+      setFilteredResults(result.data)
       setIsLoading(false)
     }
-    fetchItems()
-  }, [query])
 
+    fetchItems()
+  }, [])
+
+  const handleSearchChange = (e) => {
+    // setSearchText(e.target.value)
+    console.log(e)
+    console.log(items)
+    let tempHoldingArray = []
+    items.map((innerObj) => {
+      if (innerObj.fullName.toLowerCase().includes(e.target.value)) {
+        tempHoldingArray.push(innerObj)
+      }
+    })
+    console.log(tempHoldingArray)
+    setFilteredResults(tempHoldingArray)
+  }
 
   return (
     <>
-    <div className="container"> 
-      <Header/>
-      <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid isLoading={isLoading} items={items} />
-      <BackgroundImage/>
-    </div>
-    
+      <div className="container">
+        <Header />
+        <Search
+          getQuery={(q) => setQuery(q)}
+          searchText={searchText}
+          handleChange={handleSearchChange}
+        />
+        <CharacterGrid
+          isLoading={isLoading}
+          // items={items}
+          items={filteredResults}
+        />
+        <BackgroundImage />
+      </div>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  // only works in development runs every thing twice for robustness which includes use effect hooks re renders disable it by just commenting it out
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+)
